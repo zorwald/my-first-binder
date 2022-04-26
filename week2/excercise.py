0fd26629fbd1b08ece55bee189e86ef796ff9f28
zorwald <55626367+zorwald@users.noreply.github.com>
2022-04-26 14:02:08 +0200

    Update excercise.py

--- week2/excercise.py
+++ week2/excercise.py
@@ -1,1 +1,13 @@
 
+sqi = int(0)
+sq = []
+isl = input("Please enter an initial stock level:")
+mtp = int(input("Please enter the number of month to plan:"))
+
+print(type(mtp))
+for i in range(mtp):
+    sqi = input("Please enter the planned sales quantity:")
+    sq.append(sqi)
+    
+print(sq)    
+ 
